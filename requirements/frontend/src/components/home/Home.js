d027ef34439d8a22ece5cff7508c0251613d3359
Ellana42 <mtmrkaploun@gmail.com>
2023-06-29 15:32:08 +0200

    Home page

--- requirements/frontend/src/components/home/Home.js
+++ requirements/frontend/src/components/home/Home.js
@@ -1,6 +1,37 @@
+import  { NavLink } from "react-router-dom";
 function Home() {
     return (
-        <h1>Home</h1>
+		<div>
+		<section class="section">
+                <div class="box-main">
+                    <div class="firstHalf">
+                        <h1 class="text-big">
+						Let's Play PONG !
+                        </h1>
+                        <p class="text-small">
+						We made a great implementation of Pong you can play here !
+                        </p>
+                        <img class="pong-img" width="200px" height="100px" style={{float: 'left' }} src=
+"https://www.hiig.de/wp-content/uploads/2014/11/Pong1-1200x900.jpg" alt="Pong" />
+                    </div>
+                </div>
+            </section>
+            <section class="section">
+                <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+								Let's start playing
+                        </h1>
+						<li><NavLink to="/login">Login</NavLink></li>
+                    </div>
+                </div>
+            </section>
+            <footer className="footer">
+                <p className="text-footer">
+                    Copyright ©-All rights are reserved
+                </p>
+            </footer>
+        </div>
     )
 }
 
