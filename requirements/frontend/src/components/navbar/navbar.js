262f798d3f21d3dc2344e8b2726fbbb5032a97ae
Ellana42 <mtmrkaploun@gmail.com>
2023-06-29 10:44:57 +0200

    Added logo and styling

--- requirements/frontend/src/components/navbar/navbar.js
+++ requirements/frontend/src/components/navbar/navbar.js
@@ -2,14 +2,17 @@ import  { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className="navbar">
-            <h1>Navbar</h1>
-            <ul>
+		<nav class="navbar background">
+                <ul class="nav-list">
+                    <div class="logo">
+                        <img src=
+"https://forums.gamesalad.com/uploads/259/6I2MYLUX1J2S.jpg" alt="logo" />
+		</div>
                 <li><NavLink exact to="/">Home</NavLink></li>
                 <li><NavLink to="/contact">Contact</NavLink></li>
                 <li><NavLink to="/about">About</NavLink></li>
             </ul>
-        </div>
+		</nav>
     )
 }
 
