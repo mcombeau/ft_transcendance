c1c8e36c9ae611b0c39304abd9617b1bdb764abe
Ellana42 <mtmrkaploun@gmail.com>
2023-06-29 10:20:34 +0200

    Basic navbar working

--- requirements/frontend/src/layouts/main.js
+++ requirements/frontend/src/layouts/main.js
@@ -1,22 +1,23 @@
 import Navbar from '../components/navbar/navbar'
-import  { Routes, Route } from "react-router-dom";
-import Home from '../components/home/Home'
-import Contact from '../components/contact/Contact'
-import About from '../components/about/About'
-
+import  { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
+import Home from '../components/home/home'
+import Contact from '../components/contact/contact'
+import About from '../components/about/about'
 
 function Main() {
     return (
         <div>
+            <HashRouter>
                 <Navbar></Navbar>
                 <div className="content">
                 
                     <Routes>
-                        <Route exact path="/" component={<Home/>}/>
-                        <Route path="/contact" component={<Contact/>}/>
-                        <Route path="/about" component={<About/>}/>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route path="/about" component={About}/>
                     </Routes>
                 </div>
+            </HashRouter>
         </div>
     )
 }
