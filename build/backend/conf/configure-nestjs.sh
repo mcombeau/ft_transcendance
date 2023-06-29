1c1cfab58b2d1b4460a757cdff5c62b99ca45cbb
mcombeau <mcombeau@student.42.fr>
2023-06-29 15:12:41 +0100

    remove commented lines

--- build/backend/conf/configure-nestjs.sh
+++ build/backend/conf/configure-nestjs.sh
@@ -1,11 +1,9 @@
 echo "[Configure backend] Setting PORT 3001..."
 echo "PORT=3001" > .env
 echo "[Configure backend] Installing NestJS CLI..."
-# yarn global add @nestjs/cli
 npm install @nestjs/cli
 echo "[Configure backend] Installing app requirements..."
 npm install
-# yarn install
 # Uncomment for production
 # echo "[Configure backend] Building app..."
 # npm build
