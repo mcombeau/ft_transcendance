b2cb2c625637d8fe54c4ace630a4543c93ba183f
mcombeau <mcombeau@student.42.fr>
2023-07-02 15:37:19 +0100

    remove unecessary installs

--- build/backend/conf/configure-nestjs.sh
+++ build/backend/conf/configure-nestjs.sh
@@ -2,11 +2,11 @@ echo "[Configure backend] Installing NestJS CLI..."
 npm install -g @nestjs/cli
 
 echo "[Configure backend] Installing app requirements..."
-yarn add @nestjs/typeorm typeorm pg
 yarn install
 
 # Uncomment for production
 # echo "[Configure backend] Building app..."
 # yarn build
+
 echo "[Configure backend] Starting app..."
 yarn start:dev
