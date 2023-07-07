1b15f8d0a55299949ea2606cb1a64e1045e75967
Ellana42 <mtmrkaploun@gmail.com>
2023-07-07 15:28:13 +0200

    Message sending properly (display broken)

--- frontend-app/src/components/chat/chat.tsx
+++ frontend-app/src/components/chat/chat.tsx
@@ -1,95 +1,97 @@
-import {useContext, useEffect, useState} from 'react';
-import {WebSocketContext, WebSocketProvider, socket} from '../../contexts/WebsocketContext';
+import { useContext, useEffect, useState } from "react";
+import {
+  WebSocketContext,
+  WebSocketProvider,
+} from "../../contexts/WebsocketContext";
 
 type Message = {
-	datestamp: Date;
-	msg: string;
-	sender : string;
-	channel: string;
+  datestamp: Date;
+  msg: string;
+  sender: string;
+  channel: string;
 };
 
 export const Chat = () => {
+  const socket = useContext(WebSocketContext);
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]); // TODO: init with database
 
-		const socket = useContext(WebSocketContext);
-		const [value, setValue] = useState('');
-		const [messages, setMessage] = useState<Message[]>([]); // TODO: init with database
+  useEffect(() => {
+    socket.on("chat message", (msg: Message) => {
+      console.log("message: " + msg);
+      // var item = document.createElement('li');
+      // var messages = document.getElementById('messages');
+      // item.textContent = msg;
+      // messages.appendChild(item);
+      setMessages((prev) => [...prev, msg]);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
 
-		useEffect(() => {
-			socket.on('chat message', function(msg) {
-				console.log("message: " + msg);
-				// var item = document.createElement('li');
-				// var messages = document.getElementById('messages');
-				// item.textContent = msg;
-				// messages.appendChild(item);
-				window.scrollTo(0, document.body.scrollHeight);
-			})
+    socket.on("connect", function () {
+      console.log("I connected !");
+      // var item = document.createElement('li');
+      // var messages = document.getElementById('messages');
+      // item.textContent = "A user just connected";
+      // item.id = "event";
+      // messages.appendChild(item);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
 
-			socket.on('connect', function() {
-				console.log('I connected !');
-				// var item = document.createElement('li');
-				// var messages = document.getElementById('messages');
-				// item.textContent = "A user just connected";
-				// item.id = "event";
-				// messages.appendChild(item);
-				window.scrollTo(0, document.body.scrollHeight);
-			});
+    socket.on("connection event", function () {
+      console.log("connection");
+      // var item = document.createElement('li');
+      // var messages = document.getElementById('messages');
+      // item.textContent = "A user just connected";
+      // item.id = "event";
+      // messages.appendChild(item);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
 
-			socket.on('connection event', function() {
-				console.log('connection');
-				// var item = document.createElement('li');
-				// var messages = document.getElementById('messages');
-				// item.textContent = "A user just connected";
-				// item.id = "event";
-				// messages.appendChild(item);
-				window.scrollTo(0, document.body.scrollHeight);
-			});
-			
-			socket.on('disconnection event', function() {
-				console.log('disconnection');
-				// var item = document.createElement('li');
-				// var messages = document.getElementById('messages');
-				// item.textContent = "A user just disconnected";
-				// item.id = "event";
-				// messages.appendChild(item);
-				window.scrollTo(0, document.body.scrollHeight);
-			});
-			return () => {
-				console.log("unregistering events");
-				socket.off('chat message');
-				socket.off('connection event');
-				socket.off('disconnection event');
-				socket.off('connect');
-			};
-		}, []);
+    socket.on("disconnection event", function () {
+      console.log("disconnection");
+      // var item = document.createElement('li');
+      // var messages = document.getElementById('messages');
+      // item.textContent = "A user just disconnected";
+      // item.id = "event";
+      // messages.appendChild(item);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+    return () => {
+      console.log("unregistering events");
+      socket.off("chat message");
+      socket.off("connection event");
+      socket.off("disconnection event");
+      socket.off("connect");
+    };
+  }, []);
 
-		const handleSendMessage = (e) => {
-				var input = document.getElementById('input');
-				var messages = document.getElementById('messages');
-				e.preventDefault();
-				// if (input.value) {
+  const handleSendMessage = () => {
+    console.log("sent: " + value);
+    socket.emit("chat message", value);
+    setValue("");
+  };
 
-				// 	var item = document.createElement('li');
-				// 	item.textContent = input.value;
-				// 	item.id = "mine";
-				// 	messages.appendChild(item);
-
-				// 	socket.emit('chat message', input.value);
-
-				// 	input.value = '';
-				// 	window.scrollTo(0, document.body.scrollHeight);
-				// }
-			};
-
-    return (
-	  <WebSocketProvider value={socket}>
-	<body>
-		<ul id="messages"></ul>
-		<form id="form" onSubmit={handleSendMessage}> 
-			<input id="input" /><button>Send</button>
-		</form>
-	</body>
-	  </WebSocketProvider>
-    );
+  return (
+    <WebSocketProvider value={socket}>
+      <body>
+        <ul id="messages">
+          {messages.map((msg) => (
+            <li>{msg.msg}</li>
+          ))}
+        </ul>
+        <form id="form" onSubmit={handleSendMessage}>
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          {/* <button onClick={handleSendMessage}>Send</button> */}
+        </form>
+      </body>
+    </WebSocketProvider>
+  );
 };
 
 export default Chat;
