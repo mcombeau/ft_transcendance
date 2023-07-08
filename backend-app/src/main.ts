7712c23c97a730df06cca317ded3855425fee5b7
mcombeau <mcombeau@student.42.fr>
2023-07-08 13:34:12 +0100

    fix cors

--- backend-app/src/main.ts
+++ backend-app/src/main.ts
@@ -3,23 +3,30 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors({
-		allowedHeaders: ['content-type'],
-		origin: [
-		  'http://localhost:3000',
-		  'https://auth.42.fr',
-		  'https://auth.42.fr/',
-		  'https://api.intra.42.fr',
-		  'https://api.intra.42.fr/',
-		  'https://api.intra.42.fr/oauth/',
-		  'https://api.intra.42.fr/oauth/authorize?',
-		  'https://api.intra.42.fr/oauth/authorize?*',
-		  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-5bc97c50c4272f69f40976b44b45a630d5d9bc01a55644fbd6ab4e391c549ff5&redirect_uri=http://localhost:3001/callback&response_type=code',
-		  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-5bc97c50c4272f69f40976b44b45a630d5d9bc01a55644fbd6ab4e391c549ff5&redirect_uri=http://localhost:3001/callback&response_type=code/'
-		],
-		methods: ["GET", "POST"],
-		credentials: true,
-	  });
+	app.enableCors();
+	// app.enableCors({
+	// 	allowedHeaders: ['content-type'],
+	// 	origin: [
+	// 		'',
+	// 		'localhost',
+	// 		'http://localhost',
+	// 		'http://localhost',
+	// 		'http://localhost/backend',
+	// 		'http://localhost:3000',
+	// 		'http://localhost:80',
+	// 	  'https://auth.42.fr',
+	// 	  'https://auth.42.fr/',
+	// 	  'https://api.intra.42.fr',
+	// 	  'https://api.intra.42.fr/',
+	// 	  'https://api.intra.42.fr/oauth/',
+	// 	  'https://api.intra.42.fr/oauth/authorize?',
+	// 	  'https://api.intra.42.fr/oauth/authorize?*',
+	// 	  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-5bc97c50c4272f69f40976b44b45a630d5d9bc01a55644fbd6ab4e391c549ff5&redirect_uri=http://localhost:3001/callback&response_type=code',
+	// 	  'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-5bc97c50c4272f69f40976b44b45a630d5d9bc01a55644fbd6ab4e391c549ff5&redirect_uri=http://localhost:3001/callback&response_type=code/'
+	// 	],
+	// 	methods: ["GET", "POST"],
+	// 	credentials: true,
+	//   });
 	await app.listen(3001);
 }
 
--- frontend-app/src/components/login/login.js
+++ frontend-app/src/components/login/login.js
@@ -1,18 +1,8 @@
 function Login() {
-
-	const auth42 = () => {
-		console.log("here");
-		fetch("/auth")
-         .then((data) => {
-            console.log(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-	}
-
     return (
-		<button onClick={auth42}>Login</button>
+      <form action="/backend/auth">
+		   <button type="submit">Login</button>
+      </form>
     )
 }
 
