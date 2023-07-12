6795295887aafbba12cb08312df92e00de2b317d
Ellana42 <mtmrkaploun@gmail.com>
2023-07-12 15:49:26 +0200

    Converted play to typescript

--- frontend-app/src/components/play/play.tsx
+++ frontend-app/src/components/play/play.tsx
@@ -1,8 +1,58 @@
-import React from 'react';
-import './styles.css';
+import * as React from "react";
+import "./styles.css";
 
-class Pong extends React.Component {
-  constructor(props) {
+type Position = {
+  x: number;
+  y: number;
+};
+
+type Step = {
+  stepX: number;
+  stepY: number;
+};
+
+type State = {
+  result: number[];
+  p1: number;
+  p2: number;
+  live: boolean;
+  isPaused: boolean;
+  ballPosition: Position;
+  move: Step;
+};
+
+class Pong extends React.Component<
+  {},
+  {
+    result: number[];
+    move: Step;
+    ballPosition: Position;
+    p1: number;
+    p2: number;
+    live: boolean;
+    isPaused: boolean;
+  }
+> {
+  state: State;
+  delay: number;
+  player1x: number;
+  player2x: number;
+  pHeight: number;
+  gateHeight: number;
+  gateY: number;
+  p1GateX: number;
+  p2GateX: number;
+  playerMaxY: number;
+  playerMinY: number;
+  ballRadius: number;
+  bottomBoundary: number;
+  topBoundary: number;
+  leftBoundary: number;
+  rightBoundary: number;
+  defaultBallPosition: Position;
+  timer: NodeJS.Timeout;
+
+  constructor(props: any) {
     super(props);
     this.delay = 10;
     this.player1x = 42;
@@ -21,7 +71,7 @@ class Pong extends React.Component {
     this.rightBoundary = 710;
     this.defaultBallPosition = {
       x: 249,
-      y: 225
+      y: 225,
     };
 
     this.state = {
@@ -33,27 +83,28 @@ class Pong extends React.Component {
       ballPosition: this.defaultBallPosition,
       move: {
         stepX: -1,
-        stepY: 1
-      }
+        stepY: 1,
+      },
     };
+    // this.timer = 0;
   }
 
-  randomInitialMove() // pseudo-random ball behavior
-  {
+  randomInitialMove() {
+    // pseudo-random ball behavior
     const moves = [
       { stepX: 1, stepY: 1 },
       { stepX: 1, stepY: 2 },
       { stepX: 2, stepY: 1 },
       { stepX: -1, stepY: -1 },
-      { stepX: -1, stepY: 1 }
+      { stepX: -1, stepY: 1 },
     ];
     let initialMove = moves[Math.floor(Math.random() * moves.length)];
     console.log(initialMove);
     this.setState({ move: initialMove });
   }
 
-  checkGoals() //checking if the ball touches the borders of the goal
-  {
+  checkGoals() {
+    //checking if the ball touches the borders of the goal
     const { ballPosition } = this.state;
     if (
       ballPosition.x - this.ballRadius <= this.p1GateX + this.ballRadius * 2 &&
@@ -61,7 +112,7 @@ class Pong extends React.Component {
       ballPosition.y - this.ballRadius <= this.gateY + this.gateHeight
     ) {
       this.setState((prevState) => ({
-        result: [prevState.result[0], prevState.result[1] + 1]
+        result: [prevState.result[0], prevState.result[1] + 1],
       }));
       this.resetBall();
       this.randomInitialMove();
@@ -73,15 +124,15 @@ class Pong extends React.Component {
       ballPosition.y - this.ballRadius <= this.gateY + this.gateHeight
     ) {
       this.setState((prevState) => ({
-        result: [prevState.result[0] + 1, prevState.result[1]]
+        result: [prevState.result[0] + 1, prevState.result[1]],
       }));
       this.resetBall();
       this.randomInitialMove();
     }
   }
 
-  checkPlayers() //checking if the ball is touching the players, and if so, calculating the angle of rebound
-  {
+  checkPlayers() {
+    //checking if the ball is touching the players, and if so, calculating the angle of rebound
     const { ballPosition, p1, p2 } = this.state;
     if (
       ballPosition.x - this.ballRadius <= this.player1x &&
@@ -89,7 +140,7 @@ class Pong extends React.Component {
       ballPosition.y - this.ballRadius <= p1 + this.pHeight
     ) {
       this.setState((prevState) => ({
-        move: { stepX: -prevState.move.stepX, stepY: prevState.move.stepY }
+        move: { stepX: -prevState.move.stepX, stepY: prevState.move.stepY },
       }));
     }
 
@@ -99,13 +150,13 @@ class Pong extends React.Component {
       ballPosition.y - this.ballRadius <= p2 + this.pHeight
     ) {
       this.setState((prevState) => ({
-        move: { stepX: -prevState.move.stepX, stepY: prevState.move.stepY }
+        move: { stepX: -prevState.move.stepX, stepY: prevState.move.stepY },
       }));
     }
   }
 
-  checkBallBoundaries() //checking if the ball is touching the boundaries, and if so, calculating the angle of rebound
-  {
+  checkBallBoundaries() {
+    //checking if the ball is touching the boundaries, and if so, calculating the angle of rebound
     const { ballPosition, move } = this.state;
     const { stepX, stepY } = move;
 
@@ -114,7 +165,7 @@ class Pong extends React.Component {
       ballPosition.y - this.ballRadius + stepY <= this.topBoundary
     ) {
       this.setState((prevState) => ({
-        move: { stepX: prevState.move.stepX, stepY: -prevState.move.stepY }
+        move: { stepX: prevState.move.stepX, stepY: -prevState.move.stepY },
       }));
     }
 
@@ -123,7 +174,7 @@ class Pong extends React.Component {
       ballPosition.x + this.ballRadius + stepX >= this.rightBoundary
     ) {
       this.setState((prevState) => ({
-        move: { stepX: -prevState.move.stepX, stepY: prevState.move.stepY }
+        move: { stepX: -prevState.move.stepX, stepY: prevState.move.stepY },
       }));
     }
   }
@@ -137,169 +188,180 @@ class Pong extends React.Component {
 
   tick() {
     if (this.state.live === true) {
-      this.setState((prevState) => ({
-        ballPosition: {
-          x: prevState.ballPosition.x + prevState.move.stepX,
-          y: prevState.ballPosition.y + prevState.move.stepY
+      this.setState(
+        (prevState) => ({
+          ballPosition: {
+            x: prevState.ballPosition.x + prevState.move.stepX,
+            y: prevState.ballPosition.y + prevState.move.stepY,
+          },
+        }),
+        () => {
+          this.check();
         }
-      }), () => {
-        this.check();
-      });
+      );
     }
   }
 
-  componentDidMount() //key bindings
-  {
+  componentDidMount() {
+    //key bindings
     window.addEventListener("keydown", this.handleKeyPress);
     window.addEventListener("keydown", this.handleKeyPress2);
     this.timer = setInterval(() => this.tick(), this.delay);
   }
 
-  componentWillUnmount() //key unbindings
-  {
+  componentWillUnmount() {
+    //key unbindings
     window.removeEventListener("keydown", this.handleKeyPress);
     window.removeEventListener("keydown", this.handleKeyPress2);
     clearInterval(this.timer);
   }
 
-  checkPlayerBoundaries(player) //checking the boundaries of players for going beyond the field
-  {
+  checkPlayerBoundaries(
+    player //checking the boundaries of players for going beyond the field
+  ) {
     const { p1, p2 } = this.state;
     const { pHeight, playerMaxY, playerMinY } = this;
 
     if (player === 1) {
-      if (p1 + pHeight >= playerMaxY ) 
-        return 1;
-      if(p1 <= playerMinY) {
+      if (p1 + pHeight >= playerMaxY) return 1;
+      if (p1 <= playerMinY) {
         return 2;
-      };
+      }
     } else if (player === 2) {
-      if (p2 + pHeight >= playerMaxY)
-        return 3;
-      if(p2 <= playerMinY)
-        return 4;
+      if (p2 + pHeight >= playerMaxY) return 3;
+      if (p2 <= playerMinY) return 4;
     }
 
     return 0;
   }
 
-  resetPlayer(code) //return of players to the field, in case of exit
-  {
+  resetPlayer(
+    code //return of players to the field, in case of exit
+  ) {
     const { p1, p2 } = this.state;
     const { pHeight, playerMaxY, playerMinY } = this;
     if (code === 1) {
-      this.setState({ p1: playerMaxY - pHeight});
+      this.setState({ p1: playerMaxY - pHeight });
     }
     if (code === 2) {
-      this.setState({ p1: playerMinY});
+      this.setState({ p1: playerMinY });
     }
     if (code === 3) {
-      this.setState({ p2: playerMaxY- pHeight});
+      this.setState({ p2: playerMaxY - pHeight });
     }
     if (code === 4) {
-      this.setState({ p2: playerMinY});
+      this.setState({ p2: playerMinY });
     }
   }
 
-  handleKeyPress = (event) => //player 1 binds
-  {
+  handleKeyPress = (
+    event //player 1 binds
+  ) => {
     const step = 7;
     if (event.key === "q") {
-      this.setState((prevState) => ({
-        p1: prevState.p1 - step
-      }), () => {
-        if (this.checkPlayerBoundaries(1)) {
-          this.resetPlayer(this.checkPlayerBoundaries(1));
+      this.setState(
+        (prevState) => ({
+          p1: prevState.p1 - step,
+        }),
+        () => {
+          if (this.checkPlayerBoundaries(1)) {
+            this.resetPlayer(this.checkPlayerBoundaries(1));
+          }
         }
-      });
+      );
     } else if (event.key === "a") {
-      this.setState((prevState) => ({
-        p1: prevState.p1 + step
-      }), () => {
-        if (this.checkPlayerBoundaries(1)) {
-          this.resetPlayer(this.checkPlayerBoundaries(1));
+      this.setState(
+        (prevState) => ({
+          p1: prevState.p1 + step,
+        }),
+        () => {
+          if (this.checkPlayerBoundaries(1)) {
+            this.resetPlayer(this.checkPlayerBoundaries(1));
+          }
         }
-      });
+      );
     }
   };
 
-  handleKeyPress2 = (event) => //player 2 binds
-  {
+  handleKeyPress2 = (
+    event //player 2 binds
+  ) => {
     const step = 7;
     if (event.key === "o") {
-      this.setState((prevState) => ({
-        p2: prevState.p2 - step
-      }), () => {
-        if (this.checkPlayerBoundaries(2)) {
-          this.resetPlayer(this.checkPlayerBoundaries(2));
+      this.setState(
+        (prevState) => ({
+          p2: prevState.p2 - step,
+        }),
+        () => {
+          if (this.checkPlayerBoundaries(2)) {
+            this.resetPlayer(this.checkPlayerBoundaries(2));
+          }
         }
-      });
+      );
     } else if (event.key === "l") {
-      this.setState((prevState) => ({
-        p2: prevState.p2 + step
-      }), () => {
-        if (this.checkPlayerBoundaries(2)) {
-          this.resetPlayer(this.checkPlayerBoundaries(2));
+      this.setState(
+        (prevState) => ({
+          p2: prevState.p2 + step,
+        }),
+        () => {
+          if (this.checkPlayerBoundaries(2)) {
+            this.resetPlayer(this.checkPlayerBoundaries(2));
+          }
         }
-      });
+      );
     }
   };
 
-  resetBall() 
-  {
+  resetBall() {
     this.setState({
-      ballPosition: this.defaultBallPosition
+      ballPosition: this.defaultBallPosition,
     });
   }
 
-  restart()
-  {
+  restart() {
     this.resetBall();
     this.randomInitialMove();
   }
 
-  pause()
-  {
+  pause() {
     this.setState((prevState) => ({
       live: !prevState.live,
-      isPaused: !prevState.isPaused
+      isPaused: !prevState.isPaused,
     }));
   }
 
-  printResult1()
-  {
+  printResult1() {
     const { result } = this.state;
     return `${result[0]}`;
   }
-  printResult2()
-  {
+  printResult2() {
     const { result } = this.state;
     return `${result[1]}`;
   }
 
-  checkGameOver()
-  {
+  checkGameOver() {
     const { result } = this.state;
-    if (result[0] === 10 || result[1] === 10)
-    {
+    if (result[0] === 10 || result[1] === 10) {
       this.pause();
     }
   }
 
   render() {
-    const { ballPosition, p1, p2, live, result, } = this.state;
+    const { ballPosition, p1, p2, live, result } = this.state;
     const isPaused = this.state.isPaused;
     return (
       <div className="App">
         <div className="center-container">
-          <div className="result"><span class="res1">{this.printResult1()}</span>:<span class="res2">{this.printResult2()}</span>
+          <div className="result">
+            <span className="res1">{this.printResult1()}</span>:
+            <span className="res2">{this.printResult2()}</span>
             <div className="gameField">
               <div
                 className="ball"
                 style={{
                   top: ballPosition.y - this.ballRadius,
-                 left: ballPosition.x - this.ballRadius
-               }}
+                  left: ballPosition.x - this.ballRadius,
+                }}
               />
               <div className="midLine" />
               <div className="midLineHor" />
@@ -307,9 +369,9 @@ class Pong extends React.Component {
               <div className="player player2" style={{ top: p2 }} />
               <div className="gate gate1" />
               <div className="gate gate2" />
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
