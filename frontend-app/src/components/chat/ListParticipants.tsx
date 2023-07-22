df28d5bc34116de2e1dc69f50f0294e6859bf80f
Ellana42 <mtmrkaploun@gmail.com>
2023-07-22 16:58:18 +0200

    See participant status in participant list

--- frontend-app/src/components/chat/ListParticipants.tsx
+++ frontend-app/src/components/chat/ListParticipants.tsx
@@ -1,23 +1,38 @@
 import { NavigateFunction } from "react-router-dom";
-import { Channel, Status } from "./Chat";
+import { Channel, Status, User } from "./Chat";
 
 export const ListParticipants = (
   channel: Channel,
   navigate: NavigateFunction,
   status: Status
 ) => {
+  function displayUser(participant: User) {
+    var name = participant.username;
+    var style = {};
+    if (participant.owner) {
+      name += " ⍟";
+      style = { color: "darkcyan" };
+    } else if (participant.operator) {
+      style = { color: "darkcyan" };
+      name += " ★";
+    }
+    return (
+      <li
+        onClick={() => {
+          navigate("/user/" + participant);
+        }}
+        style={style}
+      >
+        {name}
+      </li>
+    );
+  }
   return (
     <ul className="participant_list">
       {channel.participants.map((participant) => {
         return (
           <div>
-            <li
-              onClick={() => {
-                navigate("/user/" + participant);
-              }}
-            >
-              {participant.username}
-            </li>
+            {displayUser(participant)}
             {status != Status.Normal ? (
               <div>
                 <button
