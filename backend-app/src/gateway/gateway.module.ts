c977e8a8b4690705e60f505721c99792ea07af68
mcombeau <mcombeau@student.42.fr>
2023-07-27 14:32:01 +0100

    fix dependancy issue in gateway

--- backend-app/src/gateway/gateway.module.ts
+++ backend-app/src/gateway/gateway.module.ts
@@ -6,6 +6,7 @@ import { ChatsModule } from 'src/chats/chats.module';
 import { UsersModule } from 'src/users/users.module';
 import { ChatParticipantsModule } from 'src/chat-participants/chat-participants.module';
 import { GamesModule } from 'src/games/games.module';
+import { ChatParticipantsModule } from 'src/chat-participants/chat-participants.module';
 
 @Module({
   imports: [
