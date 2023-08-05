9883e5f252ca0e56b503d8a64b2ef99b04bafe06
mcombeau <mcombeau@student.42.fr>
2023-08-05 15:05:08 +0100

    prep games database + fetch won/lost/all games by user

--- backend-app/src/games/games.service.ts
+++ backend-app/src/games/games.service.ts
@@ -1,30 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GameEntity } from 'src/games/entities/game.entity';
 import { Repository } from 'typeorm';
 import { createGameParams, updateGameParams } from './utils/types';
+import { UsersService } from 'src/users/users.service';
+import { GameCreationError } from 'src/exceptions/bad-request.interceptor';
 
 @Injectable()
 export class GamesService {
     constructor(
         @InjectRepository(GameEntity)
-        private gameRepository: Repository<GameEntity>,
+            private gameRepository: Repository<GameEntity>,
+        @Inject(forwardRef(() => UsersService))
+            private userService: UsersService,
     ) {}
 
     fetchGames() {
-        return this.gameRepository.find();
-    }
-
-    async createGame(gameDetails: createGameParams) {
-        const newGame = this.gameRepository.create({
-            ...gameDetails,
-            createdAt: new Date(),
-        });
-        return this.gameRepository.save(newGame);
+        return this.gameRepository.find({relations: ['winner', 'loser']});
     }
 
     fetchGameByID(id: number) {
         return this.gameRepository.findOne({ where: { id }});
+    }
+
+    async fetchUserAllGamesByID(userID: number) {
+        var wonGames = await this.fetchUserWonGamesByID(userID);
+        var lostGames = await this.fetchUserLostGamesByID(userID);
+
+        const allGames = wonGames.concat(lostGames);
+        return allGames;
+    }
+
+    async fetchUserWonGamesByID(userID: number) {
+        const wonGames = await this.gameRepository.find({
+            where: {
+              winner: { id: userID }
+            },
+            relations: ['winner', 'loser'],
+          });
+        return wonGames;
+    }
+
+    async fetchUserLostGamesByID(userID: number) {
+        const lostGames = await this.gameRepository.find({
+            where: {
+              loser: { id: userID }
+            },
+            relations: ['winner', 'loser'],
+          });
+        return lostGames;
+    }
+
+    async createGame(gameDetails: createGameParams) {
+        const winner = await this.userService.fetchUserByUsername(gameDetails.winnerName);
+        const loser = await this.userService.fetchUserByUsername(gameDetails.loserName);
+
+        if (!winner || !loser) {
+            throw new GameCreationError(`[Game Service]: User does not exist.`);
+        }
+
+        const newGame = this.gameRepository.create({
+            winner: { id: winner.id },
+            loser: { id: loser.id },
+            winnerScore: gameDetails.winnerScore,
+            loserScore: gameDetails.loserScore,
+            createdAt: new Date(),
+        });
+        return this.gameRepository.save(newGame);
     }
 
     updateGameByID(id: number, gameDetails: updateGameParams) {
