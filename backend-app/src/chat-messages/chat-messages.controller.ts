a09b3ba9d2fbae22e3728552812fef227d01ce74
Ellana42 <mtmrkaploun@gmail.com>
2023-08-15 17:12:19 +0200

    Added authguard to controllers

--- backend-app/src/chat-messages/chat-messages.controller.ts
+++ backend-app/src/chat-messages/chat-messages.controller.ts
@@ -6,16 +6,25 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { ChatMessagesService } from './chat-messages.service';
 import { createMessageDto } from './dtos/createMessage.dto';
 import { ChatMessageNotFoundException } from 'src/exceptions/not-found.exception';
 import { NotFoundInterceptor } from 'src/exceptions/not-found.interceptor';
-import { ApiBadRequestResponse, ApiCreatedResponse, ApiOkResponse, ApiTags, ApiUnprocessableEntityResponse } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
 import { ChatMessageEntity } from './entities/chat-message.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 // TODO: remove unecessary controller
+@UseGuards(JwtAuthGuard)
 @Controller('chat-messages')
 @ApiTags('chat-messages')
 export class ChatMessagesController {
@@ -26,21 +35,29 @@ export class ChatMessagesController {
   @ApiBadRequestResponse({ description: 'Bad request.' })
   async getMessageByID(@Param('id', ParseIntPipe) id: number) {
     const message = await this.chatMessageService.fetchMessage(id);
-    if (!message)
-      throw new ChatMessageNotFoundException(id.toString());
+    if (!message) throw new ChatMessageNotFoundException(id.toString());
     return message;
   }
 
   @Get()
-  @ApiOkResponse({ type: ChatMessageEntity, isArray: true, description: 'Get all chat messages.'})
+  @ApiOkResponse({
+    type: ChatMessageEntity,
+    isArray: true,
+    description: 'Get all chat messages.',
+  })
   getAllMessages() {
     return this.chatMessageService.fetchMessages();
   }
 
   @Post()
-  @ApiCreatedResponse({ type: ChatMessageEntity, description: 'Record created.' })
+  @ApiCreatedResponse({
+    type: ChatMessageEntity,
+    description: 'Record created.',
+  })
   @ApiBadRequestResponse({ description: 'Bad request.' })
-  @ApiUnprocessableEntityResponse({ description: 'Database error. (Unprocessable entity)' })
+  @ApiUnprocessableEntityResponse({
+    description: 'Database error. (Unprocessable entity)',
+  })
   createChatMessage(@Body() messageDto: createMessageDto) {
     return this.chatMessageService.createMessage(
       messageDto.message,
