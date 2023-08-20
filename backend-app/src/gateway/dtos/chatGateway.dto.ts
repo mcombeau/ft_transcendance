2db5da7e6a3c26591101e9703acdb6d1b1098b0a
Ellana42 <mtmrkaploun@gmail.com>
2023-08-20 17:29:16 +0200

    Reformat

--- backend-app/src/gateway/dtos/chatGateway.dto.ts
+++ backend-app/src/gateway/dtos/chatGateway.dto.ts
@@ -1,58 +1,67 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsAlphanumeric, IsInt, IsJWT, IsNumber, IsOptional, IsPositive, Length, ValidateNested } from "class-validator";
-import { createMessageDto } from "src/chat-messages/dtos/createMessage.dto";
-import { updateParticipantDto } from "src/chat-participants/dtos/updateChatParticipant.dto";
-import { createChatDto } from "src/chats/dtos/createChats.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsAlphanumeric,
+  IsInt,
+  IsJWT,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  ValidateNested,
+} from 'class-validator';
+import { createMessageDto } from 'src/chat-messages/dtos/createMessage.dto';
+import { updateParticipantDto } from 'src/chat-participants/dtos/updateChatParticipant.dto';
+import { createChatDto } from 'src/chats/dtos/createChats.dto';
 
 export class ReceivedInfoDto {
-    @ApiProperty()
-    @IsJWT()
-    token: string;
+  @ApiProperty()
+  @IsJWT()
+  token: string;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsInt()
-    @IsPositive()
-    userID?: number;
+  @ApiProperty()
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  userID?: number;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsAlphanumeric()
-    username?: string;
+  @ApiProperty()
+  @IsOptional()
+  @IsAlphanumeric()
+  username?: string;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsInt()
-    @IsPositive()
-    targetID?: number;
+  @ApiProperty()
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  targetID?: number;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsInt()
-    @IsPositive()
-    chatRoomID?: number;
+  @ApiProperty()
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  chatRoomID?: number;
 
-    @ApiProperty()
-    @IsOptional()
-    @ValidateNested()
-    @Type( () => createMessageDto )
-    messageInfo?: createMessageDto;
+  @ApiProperty()
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => createMessageDto)
+  messageInfo?: createMessageDto;
 
-    @ApiProperty()
-    @IsOptional()
-    @ValidateNested()
-    @Type( () => createChatDto )
-    chatInfo?: createChatDto;
+  @ApiProperty()
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => createChatDto)
+  chatInfo?: createChatDto;
 
-    @ApiProperty()
-    @IsOptional()
-    @ValidateNested()
-    @Type( () => updateParticipantDto )
-    participantInfo?: updateParticipantDto;
+  @ApiProperty()
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => updateParticipantDto)
+  participantInfo?: updateParticipantDto;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsNumber()
-    inviteDate?: number;
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  inviteDate?: number;
 }
+
