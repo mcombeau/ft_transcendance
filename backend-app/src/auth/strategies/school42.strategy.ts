b8e330656f2686b2bcc0a0c1ddcb3c7b54d565e8
mcombeau <mcombeau@student.42.fr>
2023-08-26 16:40:20 +0200

    refacto auth var->const

--- backend-app/src/auth/strategies/school42.strategy.ts
+++ backend-app/src/auth/strategies/school42.strategy.ts
@@ -1,10 +1,17 @@
 //TODO: Move id and secret to env variable!!!
-const CLIENT_ID ='u-s4t2ud-18f16c113212b9bfe7b0841fdf7783641ed72d9a63359b4071a723862605ceea'; // Replace with your OAuth client ID
-const CLIENT_SECRET ='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'; // Replace with your OAuth client secret
+const CLIENT_ID =
+  'u-s4t2ud-18f16c113212b9bfe7b0841fdf7783641ed72d9a63359b4071a723862605ceea'; // Replace with your OAuth client ID
+const CLIENT_SECRET =
+  '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'; // Replace with your OAuth client secret
 
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-oauth2';
-import { BadRequestException, Inject, Injectable, forwardRef } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  forwardRef,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import axios from 'axios';
 import { UserEntity } from 'src/users/entities/user.entity';
@@ -15,7 +22,8 @@ export class school42Strategy extends PassportStrategy(Strategy, '42') {
     @Inject(forwardRef(() => UsersService)) private userService: UsersService,
   ) {
     super({
-      authorizationURL: 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-18f16c113212b9bfe7b0841fdf7783641ed72d9a63359b4071a723862605ceea&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2Fcallback&response_type=code&grant_type=authorization_code',
+      authorizationURL:
+        'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-18f16c113212b9bfe7b0841fdf7783641ed72d9a63359b4071a723862605ceea&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2Fcallback&response_type=code&grant_type=authorization_code',
       tokenURL: 'https://api.intra.42.fr/oauth/token',
       clientID: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
@@ -30,10 +38,10 @@ export class school42Strategy extends PassportStrategy(Strategy, '42') {
     done: VerifyCallback,
   ): Promise<any> {
     const userResponse = await axios.get('https://api.intra.42.fr/v2/me', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     if (!profile) {
       return done(new BadRequestException(), null);
     }
@@ -44,22 +52,25 @@ export class school42Strategy extends PassportStrategy(Strategy, '42') {
       email: userResponse.data.email,
       password: null,
       // profilePicture = userInfo.image?.link
-    }
+    };
 
-    var userWithLogin = await this.userService.fetchUserBy42Login(userInfo.username);
-    var userWithUsername = await this.userService.fetchUserByUsername(userInfo.username);
-    var user: UserEntity;
+    const userWithLogin = await this.userService.fetchUserBy42Login(
+      userInfo.username,
+    );
+    const userWithUsername = await this.userService.fetchUserByUsername(
+      userInfo.username,
+    );
+    let user: UserEntity;
     if (!userWithLogin && !userWithUsername) {
       user = await this.userService.createUser({
-        ...userInfo
+        ...userInfo,
       });
-    }
-    else if (userWithUsername) {
+    } else if (userWithUsername) {
       user = userWithUsername;
-    }
-    else if (!userWithUsername && userWithLogin) {
+    } else if (!userWithUsername && userWithLogin) {
       user = userWithLogin;
     }
     done(null, user);
   }
 }
+
