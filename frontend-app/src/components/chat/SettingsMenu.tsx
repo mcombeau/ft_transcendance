c047237bbba0a92989c9b7f46d8232b123d2d855
Ellana42 <mtmrkaploun@gmail.com>
2023-09-05 16:56:55 +0200

    Removed todo

--- frontend-app/src/components/chat/SettingsMenu.tsx
+++ frontend-app/src/components/chat/SettingsMenu.tsx
@@ -100,7 +100,6 @@ export const SettingsMenu = (
           )}
         </div>
       );
-      // TODO: fix this button
       var private_public = (
         <div>
           <label className="switch">
