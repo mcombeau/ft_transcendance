bd44e8cb336f64d4a111d631bad04fd6634420ea
Ellana42 <mtmrkaploun@gmail.com>
2023-10-24 17:47:13 +0200

    Move 2fa logic to settings pannel

--- frontend-app/src/components/profile/profile.tsx
+++ frontend-app/src/components/profile/profile.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { useCookies } from "react-cookie";
 import { getIs2faEnabled, getUserID } from "../../cookies";
 import { WebSocketContext } from "../../contexts/WebsocketContext";
-import { QRCodeRaw } from "@cheprasov/qrcode";
 import defaultProfilePicture from "./profilePicture.jpg";
 import FriendsList from "./friendsList";
 import Stats from "./stats";
@@ -45,39 +44,6 @@ function Profile() {
   const [cookies] = useCookies(["cookie-name"]);
   const socket = useContext(WebSocketContext);
 
-  async function enable2Fa() {
-    var result: Uint8Array;
-    var request = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${cookies["token"]}`,
-      },
-    };
-    await fetch("http://localhost:3001/auth/2fa/generate", request).then(
-      async (response) => {
-        const data = await response.json();
-        if (!response.ok) {
-          console.log("error QR code generation");
-          return;
-        }
-        // TODO display QR code somehow
-        console.log("GENERATE");
-        const code = new QRCodeRaw(response["body"]);
-        console.log(code);
-      }
-    );
-    // TODO: post request to generate
-    // TODO: display QR code + field for code with submit
-    // TODO: post it to turn on and if it works close everything
-  }
-
-  function disable2Fa() {
-    // TODO: post request to turn off
-    // TODO: if it works flip the switch
-    // TODO: check if cookie is up to date
-  }
-
   useEffect(() => {
     var request = {
       headers: {
@@ -115,13 +81,6 @@ function Profile() {
     return <h1>No such user</h1>;
   }
 
-  // TODO: put 2fa checkbox in edit profile menu
-  // <input
-  //   type="checkbox"
-  //   checked={is2faEnabled}
-  //   onChange={() => {
-  //     enable2Fa();
-  //   }} />
   return (
     <div>
       <img src={defaultProfilePicture} width="100" height="100"></img>
--- frontend-app/src/components/profile/profileSettings.tsx
+++ frontend-app/src/components/profile/profileSettings.tsx
@@ -1,4 +1,45 @@
-function ProfileSettings() {
+import { QRCodeRaw } from "@cheprasov/qrcode";
+
+function ProfileSettings(cookies: any) {
+  // TODO: put 2fa checkbox in edit profile menu
+  // <input
+  //   type="checkbox"
+  //   checked={is2faEnabled}
+  //   onChange={() => {
+  //     enable2Fa();
+  //   }} />
+  async function enable2Fa() {
+    var result: Uint8Array;
+    var request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies["token"]}`,
+      },
+    };
+    await fetch("http://localhost:3001/auth/2fa/generate", request).then(
+      async (response) => {
+        const data = await response.json();
+        if (!response.ok) {
+          console.log("error QR code generation");
+          return;
+        }
+        // TODO display QR code somehow
+        console.log("GENERATE");
+        const code = new QRCodeRaw(response["body"]);
+        console.log(code);
+      }
+    );
+    // TODO: post request to generate
+    // TODO: display QR code + field for code with submit
+    // TODO: post it to turn on and if it works close everything
+  }
+
+  function disable2Fa() {
+    // TODO: post request to turn off
+    // TODO: if it works flip the switch
+    // TODO: check if cookie is up to date
+  }
   return;
 }
 
