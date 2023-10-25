0372cb28babe4bfc464090610aeb7754bb0c386c
Ellana42 <mtmrkaploun@gmail.com>
2023-10-25 17:21:46 +0200

    WIP bug change username email

--- frontend-app/src/components/profile/profileSettings.tsx
+++ frontend-app/src/components/profile/profileSettings.tsx
@@ -17,9 +17,11 @@ function ProfileSettings(
   //     enable2Fa();
   //   }} />
   const [newUsername, setNewUsername] = useState("");
+  const [newEmail, setNewEmail] = useState("");
 
   useEffect(() => {
     if (user !== undefined) setNewUsername(user.username);
+    if (user !== undefined) setNewEmail(user.email);
   }, [user]);
 
   async function enable2Fa() {
@@ -55,16 +57,22 @@ function ProfileSettings(
     // TODO: check if cookie is up to date
   }
 
-  function submitUsername(e: any) {
+  function submitUserInfo(e: any) {
     var request = {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${cookies["token"]}`,
       },
+      body: JSON.stringify({
+        username: newUsername,
+        email: newEmail,
+      }),
     };
     e.preventDefault();
     console.log("Submitting username " + newUsername);
+    console.log("Submitting email " + newEmail);
+    console.log("request", request);
     fetch(`http://localhost:3001/users/${getUserID(cookies)}`, request).then(
       async (response) => {
         if (!response.ok) alert("There was a problem with updating settings");
@@ -83,11 +91,17 @@ function ProfileSettings(
       >
         Close settings
       </button>
-      <form className="edit_field" onSubmit={submitUsername}>
+      <form className="edit_field" onSubmit={submitUserInfo}>
         <input
           value={newUsername}
           onChange={(e) => {
             setNewUsername(e.target.value);
+          }}
+        />
+        <input
+          value={newEmail}
+          onChange={(e) => {
+            setNewEmail(e.target.value);
           }}
         />
         <button>Save changes</button>
