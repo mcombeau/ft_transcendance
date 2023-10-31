b54adb7cabae055b7f18d22f7fc3004ed770986c
Ellana42 <mtmrkaploun@gmail.com>
2023-10-31 14:35:05 +0100

    Go back to home page after logging out

--- frontend-app/src/components/logout.tsx
+++ frontend-app/src/components/logout.tsx
@@ -1,15 +1,17 @@
 import { useContext, useEffect } from "react";
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import { AuthenticationContext } from "./authenticationState";
 
 function Logout() {
   const [cookies, setCookie, removeCookie] = useCookies();
   const { userID, setUserID } = useContext(AuthenticationContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log("Removing cookie");
     setUserID(null);
     removeCookie("token", { path: "/" });
+    navigate("/");
   }, []);
 }
 export default Logout;
