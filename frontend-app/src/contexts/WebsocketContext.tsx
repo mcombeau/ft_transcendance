02e0113157087106c2d6e2fc0ff93cb03a0a6c6c
mcombeau <mcombeau@student.42.fr>
2023-11-09 15:15:19 +0100

    add todo

--- frontend-app/src/contexts/WebsocketContext.tsx
+++ frontend-app/src/contexts/WebsocketContext.tsx
@@ -3,6 +3,7 @@ import { io, Socket } from "socket.io-client";
 
 const URL = "http://localhost:3001";
 
+// TODO: make token getting dynamic so that token is available in backend socket
 const token = document.cookie.split("=")[1]; // Maybe clean up later
 
 export const socket = io(URL, {
