9df6d7533e940770f99df3d474ae7b456889523c
mcombeau <mcombeau@student.42.fr>
2023-11-13 16:01:02 +0100

    fix delete old avatar url

--- backend-app/src/users/users.service.ts
+++ backend-app/src/users/users.service.ts
@@ -173,7 +173,7 @@ export class UsersService {
 
   async saveUserAvatarByUserID(id: number, file: Express.Multer.File) {
     const user = await this.fetchUserByID(id);
-    await removeUserAvatarByUserID(id);
+    await this.unlinkAvatar(user.avatarUrl);
     const filename = user.id + extname(file.originalname);
     const filepath = join(process.cwd(), 'user_data', filename);
 
