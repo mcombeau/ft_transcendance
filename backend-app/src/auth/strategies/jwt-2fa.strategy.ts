436456c6da3c23756bcc60c04a696eb5955d6bda
mcombeau <mcombeau@student.42.fr>
2023-11-14 18:14:03 +0100

    TODO: remove this secret? Also, why secret is literally secret?

--- backend-app/src/auth/strategies/jwt-2fa.strategy.ts
+++ backend-app/src/auth/strategies/jwt-2fa.strategy.ts
@@ -1,19 +1,21 @@
-import {ExtractJwt, Strategy} from 'passport-jwt';
-import {PassportStrategy} from '@nestjs/passport';
-import {Injectable} from '@nestjs/common';
-import {UsersService} from '../../users/users.service';
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "@nestjs/common";
+import { UsersService } from "../../users/users.service";
 
+// TODO [iazimzha]: WTF is secretOrKey: 'secret' ????? Do we need to change this ???
+// TODO [mcombeau]: Also put that secret in env file??
 @Injectable()
-export class Jwt2faStrategy extends PassportStrategy(Strategy, 'jwt-2fa') {
+export class Jwt2faStrategy extends PassportStrategy(Strategy, "jwt-2fa") {
 	constructor(private readonly userService: UsersService) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: 'secret',
+			secretOrKey: "secret",
 		});
 	}
 
 	async validate(payload: any) {
-		console.log('[JWT-2FA Auth Guard] payload:', payload);
+		console.log("[JWT-2FA Auth Guard] payload:", payload);
 		const user = await this.userService.fetchUserByUsername(payload.username);
 
 		if (!user.isTwoFactorAuthenticationEnabled) {
