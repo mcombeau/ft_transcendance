16d4f439211994e983b0cad643b18bf265ac7dfc
mcombeau <mcombeau@student.42.fr>
2023-11-15 16:41:10 +0100

    add server-side validation for avatars

--- backend-app/src/users/users.controller.ts
+++ backend-app/src/users/users.controller.ts
@@ -11,41 +11,45 @@ import {
 	UploadedFile,
 	UseInterceptors,
 	Res,
-} from '@nestjs/common';
+	ParseFilePipe,
+	MaxFileSizeValidator,
+	FileTypeValidator,
+} from "@nestjs/common";
 import {
 	ApiBadRequestResponse,
 	ApiCreatedResponse,
 	ApiOkResponse,
 	ApiTags,
 	ApiUnprocessableEntityResponse,
-} from '@nestjs/swagger';
-import {JwtAuthGuard} from 'src/auth/guards/jwt-auth.guard';
-import {UserNotFoundException} from 'src/exceptions/not-found.exception';
-import {createUsersDto} from 'src/users/dtos/createUsers.dto';
-import {updateUsersDto} from 'src/users/dtos/updateUsers.dto';
-import {UsersService} from 'src/users/users.service';
-import {sendBlockedUserDto} from 'src/blocked-users/dtos/sendBlockedUser.dto';
-import {UserEntity} from './entities/user.entity';
-import {ChatEntity} from 'src/chats/entities/chat.entity';
-import {sendGameDto} from 'src/games/dtos/sendGame.dto';
-import {sendFriendDto} from 'src/friends/dtos/sendFriend.dto';
-import {UpdateResult, DeleteResult} from 'typeorm';
-import {FileInterceptor} from '@nestjs/platform-express';
-import {Response, Express} from 'express';
+} from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { UserNotFoundException } from "src/exceptions/not-found.exception";
+import { createUsersDto } from "src/users/dtos/createUsers.dto";
+import { updateUsersDto } from "src/users/dtos/updateUsers.dto";
+import { UsersService } from "src/users/users.service";
+import { sendBlockedUserDto } from "src/blocked-users/dtos/sendBlockedUser.dto";
+import { UserEntity } from "./entities/user.entity";
+import { ChatEntity } from "src/chats/entities/chat.entity";
+import { sendGameDto } from "src/games/dtos/sendGame.dto";
+import { sendFriendDto } from "src/friends/dtos/sendFriend.dto";
+import { UpdateResult, DeleteResult } from "typeorm";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Response, Express } from "express";
+import { max } from "rxjs";
 
-@ApiTags('users')
-@Controller('users')
+@ApiTags("users")
+@Controller("users")
 export class UsersController {
 	constructor(private userService: UsersService) {}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id')
+	@Get(":id")
 	@ApiOkResponse({
 		type: UserEntity,
-		description: 'Get user by ID.',
+		description: "Get user by ID.",
 	})
 	async getUserByID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<UserEntity> {
 		const user = await this.userService.fetchUserByID(id);
 		if (!user) throw new UserNotFoundException(id.toString());
@@ -53,78 +57,78 @@ export class UsersController {
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id/chats')
+	@Get(":id/chats")
 	@ApiOkResponse({
 		type: ChatEntity,
 		isArray: true,
-		description: 'Get user chats (including DMs) by user ID.',
+		description: "Get user chats (including DMs) by user ID.",
 	})
 	async getUserChatsByUserID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<ChatEntity[]> {
 		return this.userService.fetchUserChatsByUserID(id);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id/DMs')
+	@Get(":id/DMs")
 	@ApiOkResponse({
 		type: ChatEntity,
 		isArray: true,
-		description: 'Get user DM chats (only DMs) by user ID.',
+		description: "Get user DM chats (only DMs) by user ID.",
 	})
 	async getUserChatDMsByUserID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<ChatEntity[]> {
 		return this.userService.fetchUserChatDMsByUserID(id);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id/games')
+	@Get(":id/games")
 	@ApiOkResponse({
 		type: sendGameDto,
 		isArray: true,
-		description: 'Get user games by user ID.',
+		description: "Get user games by user ID.",
 	})
 	async getUserGamesByUserID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<sendGameDto[]> {
 		return this.userService.fetchUserGamesByUserID(id);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id/friends')
+	@Get(":id/friends")
 	@ApiOkResponse({
 		type: sendFriendDto,
 		isArray: true,
-		description: 'Get user friends by user ID.',
+		description: "Get user friends by user ID.",
 	})
 	async getUserFriendsByUserID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<sendFriendDto[]> {
 		return this.userService.fetchUserFriendsByUserID(id);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id/blockedUsers')
+	@Get(":id/blockedUsers")
 	@ApiOkResponse({
 		type: sendBlockedUserDto,
 		isArray: true,
-		description: 'Get blocked user list by user ID.',
+		description: "Get blocked user list by user ID.",
 	})
 	async getUserBlockedUsersByUserID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<sendBlockedUserDto[]> {
 		return this.userService.fetchUserBlockedUsersByUserID(id);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Get(':id/avatar')
+	@Get(":id/avatar")
 	@ApiOkResponse({
-		description: 'Get avatar by user ID.',
+		description: "Get avatar by user ID.",
 	})
 	async getUserAvatarByUserID(
-		@Param('id', ParseIntPipe) id: number,
-		@Res() res: Response,
+		@Param("id", ParseIntPipe) id: number,
+		@Res() res: Response
 	) {
 		const file = await this.userService.fetchUserAvatarByUserID(id);
 		file.pipe(res);
@@ -135,7 +139,7 @@ export class UsersController {
 	@ApiOkResponse({
 		type: UserEntity,
 		isArray: true,
-		description: 'Get all users.',
+		description: "Get all users.",
 	})
 	getUsers(): Promise<UserEntity[]> {
 		return this.userService.fetchUsers();
@@ -143,54 +147,62 @@ export class UsersController {
 
 	// TODO: Make sure you can't change someone else's avatar
 	@UseGuards(JwtAuthGuard)
-	@Post(':id/avatar')
-	@UseInterceptors(FileInterceptor('file'))
+	@Post(":id/avatar")
+	@UseInterceptors(FileInterceptor("file"))
 	async uploadUserAvatarByUserID(
-		@Param('id', ParseIntPipe) id: number,
-		@UploadedFile() file: Express.Multer.File,
+		@Param("id", ParseIntPipe) id: number,
+		@UploadedFile(
+			new ParseFilePipe({
+				validators: [
+					new MaxFileSizeValidator({ maxSize: 50000000 }),
+					new FileTypeValidator({ fileType: ".(png|jpeg|jpg)" }),
+				],
+			})
+		)
+		file: Express.Multer.File
 	) {
-		console.log('[User Controller] Uploading file', file);
+		console.log("[User Controller] Uploading file", file);
 		await this.userService.saveUserAvatarByUserID(id, file);
 	}
 
 	@Post()
-	@ApiCreatedResponse({type: UserEntity, description: 'Record created.'})
-	@ApiBadRequestResponse({description: 'Bad request.'})
+	@ApiCreatedResponse({ type: UserEntity, description: "Record created." })
+	@ApiBadRequestResponse({ description: "Bad request." })
 	@ApiUnprocessableEntityResponse({
-		description: 'Database error. (Unprocessable entity)',
+		description: "Database error. (Unprocessable entity)",
 	})
 	createUser(@Body() userDto: createUsersDto): Promise<UserEntity> {
 		return this.userService.createUser(userDto);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Patch(':id')
-	@ApiCreatedResponse({description: 'Record updated.'})
-	@ApiBadRequestResponse({description: 'Bad request'})
+	@Patch(":id")
+	@ApiCreatedResponse({ description: "Record updated." })
+	@ApiBadRequestResponse({ description: "Bad request" })
 	@ApiUnprocessableEntityResponse({
-		description: 'Database error. (Unprocessable entity)',
+		description: "Database error. (Unprocessable entity)",
 	})
 	async updateUserByID(
-		@Param('id', ParseIntPipe) id: number,
-		@Body() updateUserDto: updateUsersDto,
+		@Param("id", ParseIntPipe) id: number,
+		@Body() updateUserDto: updateUsersDto
 	): Promise<UpdateResult> {
 		return this.userService.updateUserByID(id, updateUserDto);
 	}
 
 	@UseGuards(JwtAuthGuard)
-	@Delete(':id/avatar')
-	async deleteUserAvatarByUserID(@Param('id', ParseIntPipe) id: number) {
+	@Delete(":id/avatar")
+	async deleteUserAvatarByUserID(@Param("id", ParseIntPipe) id: number) {
 		return this.userService.removeUserAvatarByUserID(id);
 	}
 
-	@Delete(':id')
-	@ApiOkResponse({description: 'Record deleted by ID.'})
-	@ApiBadRequestResponse({description: 'Bad request'})
+	@Delete(":id")
+	@ApiOkResponse({ description: "Record deleted by ID." })
+	@ApiBadRequestResponse({ description: "Bad request" })
 	@ApiUnprocessableEntityResponse({
-		description: 'Database error. (Unprocessable entity)',
+		description: "Database error. (Unprocessable entity)",
 	})
 	async deleteUserByID(
-		@Param('id', ParseIntPipe) id: number,
+		@Param("id", ParseIntPipe) id: number
 	): Promise<DeleteResult> {
 		return this.userService.deleteUserByID(id);
 	}
--- backend-app/src/users/users.service.ts
+++ backend-app/src/users/users.service.ts
@@ -1,23 +1,29 @@
-import {Inject, Injectable, forwardRef} from '@nestjs/common';
-import {createReadStream, writeFile, unlink, existsSync} from 'fs';
-import {InjectRepository} from '@nestjs/typeorm';
-import {ChatEntity} from 'src/chats/entities/chat.entity';
-import {UserNotFoundError} from 'src/exceptions/not-found.interceptor';
-import {PasswordService} from 'src/password/password.service';
-import {UserEntity} from 'src/users/entities/user.entity';
-import {createUserParams} from 'src/users/utils/types';
-import {updateUserParams} from 'src/users/utils/types';
-import {Repository, UpdateResult, DeleteResult} from 'typeorm';
-import {ChatsService} from 'src/chats/chats.service';
-import {GamesService} from 'src/games/games.service';
-import {FriendsService} from 'src/friends/friends.service';
-import {BlockedUsersService} from 'src/blocked-users/blockedUsers.service';
-import {sendParticipantDto} from 'src/chat-participants/dtos/sendChatParticipant.dto';
-import {BadRequestException} from '@nestjs/common';
-import {sendGameDto} from 'src/games/dtos/sendGame.dto';
-import {sendFriendDto} from 'src/friends/dtos/sendFriend.dto';
-import {sendBlockedUserDto} from 'src/blocked-users/dtos/sendBlockedUser.dto';
-import {join, extname} from 'path';
+import { Inject, Injectable, forwardRef } from "@nestjs/common";
+import {
+	createReadStream,
+	writeFile,
+	unlink,
+	existsSync,
+	ReadStream,
+} from "fs";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ChatEntity } from "src/chats/entities/chat.entity";
+import { UserNotFoundError } from "src/exceptions/not-found.interceptor";
+import { PasswordService } from "src/password/password.service";
+import { UserEntity } from "src/users/entities/user.entity";
+import { createUserParams } from "src/users/utils/types";
+import { updateUserParams } from "src/users/utils/types";
+import { Repository, UpdateResult, DeleteResult } from "typeorm";
+import { ChatsService } from "src/chats/chats.service";
+import { GamesService } from "src/games/games.service";
+import { FriendsService } from "src/friends/friends.service";
+import { BlockedUsersService } from "src/blocked-users/blockedUsers.service";
+import { sendParticipantDto } from "src/chat-participants/dtos/sendChatParticipant.dto";
+import { BadRequestException } from "@nestjs/common";
+import { sendGameDto } from "src/games/dtos/sendGame.dto";
+import { sendFriendDto } from "src/friends/dtos/sendFriend.dto";
+import { sendBlockedUserDto } from "src/blocked-users/dtos/sendBlockedUser.dto";
+import { join, extname } from "path";
 
 @Injectable()
 export class UsersService {
@@ -33,10 +39,10 @@ export class UsersService {
 		@Inject(forwardRef(() => FriendsService))
 		private friendService: FriendsService,
 		@Inject(forwardRef(() => BlockedUsersService))
-		private blockedUserService: BlockedUsersService,
+		private blockedUserService: BlockedUsersService
 	) {}
 
-	defaultAvatarURL = 'src/images/defaultProfilePicture.jpg';
+	defaultAvatarURL = "src/images/defaultProfilePicture.jpg";
 
 	fetchUsers(): Promise<UserEntity[]> {
 		return this.userRepository.find();
@@ -44,27 +50,27 @@ export class UsersService {
 
 	fetchUserByID(id: number): Promise<UserEntity> {
 		return this.userRepository.findOne({
-			where: {id},
+			where: { id },
 		});
 	}
 
 	fetchUserByUsername(username: string): Promise<UserEntity> {
 		return this.userRepository.findOne({
-			where: {username: username},
+			where: { username: username },
 		});
 	}
 
 	async fetchUserBy42Login(login: string): Promise<UserEntity> {
 		const user = await this.userRepository.findOne({
-			where: {login42: login},
+			where: { login42: login },
 		});
 		return user;
 	}
 
 	async fetchUserChatsByUserID(userID: number): Promise<ChatEntity[]> {
 		const user = await this.userRepository.findOne({
-			where: {id: userID},
-			relations: ['chatRooms.chatRoom'],
+			where: { id: userID },
+			relations: ["chatRooms.chatRoom"],
 		});
 		if (!user || user === undefined || user === null) {
 			throw new UserNotFoundError();
@@ -93,15 +99,15 @@ export class UsersService {
 	}
 
 	async fetchUserBlockedUsersByUserID(
-		userID: number,
+		userID: number
 	): Promise<sendBlockedUserDto[]> {
 		return this.blockedUserService.fetchBlockedUsersByUserID(userID);
 	}
 
 	async fetchUserChatDMsByUserID(id: number): Promise<ChatEntity[]> {
 		const user = await this.userRepository.findOne({
-			where: {id},
-			relations: ['chatRooms.chatRoom'],
+			where: { id },
+			relations: ["chatRooms.chatRoom"],
 		});
 		const userDMRooms: ChatEntity[] = [];
 		for (const e of user.chatRooms) {
@@ -112,29 +118,35 @@ export class UsersService {
 		return userDMRooms;
 	}
 
-	async fetchUserAvatarByUserID(id: number) {
+	async fetchUserAvatarByUserID(id: number): Promise<ReadStream> {
 		const user = await this.fetchUserByID(id);
 		const filename = join(process.cwd(), user.avatarUrl);
 
-		let file;
+		let file: ReadStream;
 		if (existsSync(filename)) {
+			console.log("---------- File", filename, "exists");
 			file = createReadStream(filename);
 			return file;
 		} else {
+			console.log(
+				"----------- File",
+				filename,
+				"did not exists, defaulting default profile picture"
+			);
 			file = createReadStream(join(process.cwd(), this.defaultAvatarURL));
 			return file;
 		}
 	}
 
 	async createUser(userDetails: createUserParams): Promise<UserEntity> {
-		const hashedPassword = await this.passwordService.hashPassword(
-			userDetails.password,
+		const hashedPassword: string = await this.passwordService.hashPassword(
+			userDetails.password
 		);
 		userDetails.password = hashedPassword;
-		const newUserInfo = this.userRepository.create({
+		const newUserInfo: UserEntity = this.userRepository.create({
 			...userDetails,
 			isTwoFactorAuthenticationEnabled: false,
-			twoFactorAuthenticationSecret: '',
+			twoFactorAuthenticationSecret: "",
 			createdAt: new Date(),
 			avatarUrl: this.defaultAvatarURL,
 		});
@@ -144,65 +156,93 @@ export class UsersService {
 
 	async getUserPasswordHash(userID: number): Promise<string> {
 		const user = await this.userRepository.findOne({
-			where: {id: userID},
-			select: ['password'],
+			where: { id: userID },
+			select: ["password"],
 		});
 		return user.password;
 	}
 
-	private async unlinkAvatar(filename: string) {
+	private unlinkAvatar(filename: string) {
 		if (filename === this.defaultAvatarURL) {
 			return;
 		}
-		await unlink(filename, (err) => {
+		unlink(filename, (err) => {
 			if (err) {
 				console.log(
-					'[User Service][Remove avatar] Failed to remove avatar...',
-					filename,
+					"[User Service][Remove avatar] Failed to remove avatar...",
+					filename
 				);
 			} else {
 				console.log(
-					'[User Service][Remove avatar] Avatar removed successfully !',
-					filename,
+					"[User Service][Remove avatar] Avatar removed successfully !",
+					filename
 				);
 			}
 		});
 	}
 
-	private async deleteUserAvatarFileByID(id: number) {
-		await this.unlinkAvatar(join(process.cwd(), 'user_data', id + '.png'));
-		await this.unlinkAvatar(join(process.cwd(), 'user_data', id + '.jpg'));
-		await this.unlinkAvatar(join(process.cwd(), 'user_data', id + '.jpeg'));
+	private deleteUserAvatarFileByID(id: number) {
+		this.unlinkAvatar(join(process.cwd(), "user_data", id + ".png"));
+		this.unlinkAvatar(join(process.cwd(), "user_data", id + ".jpg"));
+		this.unlinkAvatar(join(process.cwd(), "user_data", id + ".jpeg"));
 	}
 
 	async removeUserAvatarByUserID(id: number) {
-		await this.deleteUserAvatarFileByID(id);
+		this.deleteUserAvatarFileByID(id);
 		await this.updateUserByID(id, {
 			avatarUrl: this.defaultAvatarURL,
 		});
 	}
 
+	private async checkAvatarIsValidImage(file: Express.Multer.File) {
+		const { fileTypeFromBuffer } = await (eval(
+			'import("file-type")'
+		) as Promise<typeof import("file-type")>);
+
+		const type = await fileTypeFromBuffer(file.buffer);
+		console.log("[User Service] Actual file type:", type);
+		if (!type) {
+			throw new BadRequestException("Invalid file type");
+		}
+		if (type.mime !== "image/png" && type.mime !== "image/jpeg") {
+			throw new BadRequestException("Invalid file type");
+		}
+	}
+
+	private async getAvatarFileExtension(file: Express.Multer.File) {
+		const { fileTypeFromBuffer } = await (eval(
+			'import("file-type")'
+		) as Promise<typeof import("file-type")>);
+
+		const type = await fileTypeFromBuffer(file.buffer);
+		if (!type || !type.ext) {
+			throw new BadRequestException("Invalid file type");
+		}
+		return type.ext;
+	}
+
 	async saveUserAvatarByUserID(id: number, file: Express.Multer.File) {
 		const user = await this.fetchUserByID(id);
-		await this.unlinkAvatar(user.avatarUrl);
-		const filename = user.id + extname(file.originalname);
-		const filepath = join(process.cwd(), 'user_data', filename);
+		await this.checkAvatarIsValidImage(file);
+		this.unlinkAvatar(user.avatarUrl);
+		const filename = user.id + "." + (await this.getAvatarFileExtension(file));
+		const filepath = join(process.cwd(), "user_data", filename);
 
-		await writeFile(filepath, file.buffer, 'binary', (err) => {
+		writeFile(filepath, file.buffer, "binary", (err) => {
 			if (!err)
 				console.log(
-					'[User Service][Upload avatar] Avatar uploaded successfully !',
+					"[User Service][Upload avatar] Avatar uploaded successfully !",
 					filename,
-					'at path',
-					filepath,
+					"at path",
+					filepath
 				);
 		});
-		await this.updateUserByID(user.id, {avatarUrl: 'user_data/' + filename});
+		await this.updateUserByID(user.id, { avatarUrl: "user_data/" + filename });
 	}
 
 	async updateUserByID(
 		id: number,
-		userDetails: updateUserParams,
+		userDetails: updateUserParams
 	): Promise<UpdateResult> {
 		const updatedInfo: updateUserParams = {};
 		if (userDetails.username) updatedInfo.username = userDetails.username;
@@ -210,10 +250,10 @@ export class UsersService {
 		if (userDetails.avatarUrl) updatedInfo.avatarUrl = userDetails.avatarUrl;
 		if (userDetails.status) {
 			console.log(
-				'[User Service]: updating user',
+				"[User Service]: updating user",
 				id,
-				'status to ',
-				userDetails.status,
+				"status to ",
+				userDetails.status
 			);
 			updatedInfo.status = userDetails.status;
 		}
@@ -222,37 +262,37 @@ export class UsersService {
 			const user = await this.fetchUserByID(id);
 			const isValidCurrentPassword = await this.passwordService.checkPassword(
 				userDetails.currentPassword,
-				user,
+				user
 			);
 			if (isValidCurrentPassword) {
 				const hashedPassword = await this.passwordService.hashPassword(
-					userDetails.newPassword,
+					userDetails.newPassword
 				);
 				updatedInfo.password = hashedPassword;
 			} else {
-				throw new BadRequestException('Invalid password');
+				throw new BadRequestException("Invalid password");
 			}
 		}
-		return this.userRepository.update({id}, {...updatedInfo});
+		return this.userRepository.update({ id }, { ...updatedInfo });
 	}
 
 	async setTwoFactorAuthenticationSecret(secret: string, id: number) {
 		return this.userRepository.update(
-			{id},
-			{twoFactorAuthenticationSecret: secret},
+			{ id },
+			{ twoFactorAuthenticationSecret: secret }
 		);
 	}
 
 	async setTwoFactorAuthentication(username: string, state: boolean) {
 		const user = await this.fetchUserByUsername(username);
 		await this.userRepository.update(
-			{id: user.id},
-			{isTwoFactorAuthenticationEnabled: state},
+			{ id: user.id },
+			{ isTwoFactorAuthenticationEnabled: state }
 		);
 	}
 
 	async deleteUserByID(id: number): Promise<DeleteResult> {
 		await this.removeUserAvatarByUserID(id);
-		return this.userRepository.delete({id});
+		return this.userRepository.delete({ id });
 	}
 }
