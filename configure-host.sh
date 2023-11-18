1f4e66625c5ad327d1d98a69a59332ca02f50c1f
mcombeau <mcombeau@student.42.fr>
2023-11-18 18:22:16 +0100

    change name of HOST to FT_TRANSCENDANCE_HOST because otherwise Nginx gets confused

--- configure-host.sh
+++ configure-host.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "[Configure host] Adding host to /etc/hosts..."
-if [ ${HOST} == "localhost" ]; then
-	echo "[Configure host] Host is ${HOST} (localhost)."
+if [ ${FT_TRANSCENDANCE_HOST} == "localhost" ]; then
+	echo "[Configure host] Host is ${FT_TRANSCENDANCE_HOST} (localhost)."
 	exit 0
 fi
-if ! grep -q ${HOST} "/etc/hosts"; then
-	echo "127.0.0.1 ${HOST}" | sudo tee -a /etc/hosts
-	echo "[Configure host] Added ${HOST} to /etc/hosts."
+if ! grep -q ${FT_TRANSCENDANCE_HOST} "/etc/hosts"; then
+	echo "127.0.0.1 ${FT_TRANSCENDANCE_HOST}" | sudo tee -a /etc/hosts
+	echo "[Configure host] Added ${FT_TRANSCENDANCE_HOST} to /etc/hosts."
 else
-	echo "[Configure host] ${HOST} already in /etc/hosts."
+	echo "[Configure host] ${FT_TRANSCENDANCE_HOST} already in /etc/hosts."
 fi
