be44ed16f1db0b41e5f66f9a006c1ce0d2e2165e
Ellana42 <mtmrkaploun@gmail.com>
2023-11-24 16:25:52 +0100

    Css for homepage

--- frontend-app/src/components/home/home.tsx
+++ frontend-app/src/components/home/home.tsx
@@ -1,19 +1,16 @@
 function Home() {
 	return (
-		<div>
-			<section className="section">
-				<div className="box-main">
-					<div className="firstHalf">
-						<h1 className="text-big">Let's Play PONG !</h1>
-						<p className="">
-							We made a great implementation of Pong you can play here !
-						</p>
-					</div>
-				</div>
-			</section>
-			<footer className="footer">
-				<p className="text-footer">Copyright ©-All rights are reserved</p>
-			</footer>
+		<div className="bg-lightblue rounded-lg m-4 p-4 text-darkblue">
+			<h1 className="font-bold text-lg mb-4">Let's Play PONG !</h1>
+			<p className="">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.
+			</p>
 		</div>
 	);
 }
