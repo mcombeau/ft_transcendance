58a89b6dc7afc11eb189dddcfa375e2c834a11b2
mcombeau <mcombeau@student.42.fr>
2023-11-28 18:12:18 +0100

    fix validation on DM chat name

--- backend-app/src/chats/chats.service.ts
+++ backend-app/src/chats/chats.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, forwardRef } from "@nestjs/common";
+import { Inject, Injectable, forwardRef, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ChatEntity } from "src/chats/entities/chat.entity";
 import { Repository, UpdateResult, DeleteResult } from "typeorm";
@@ -37,6 +37,8 @@ export class ChatsService {
 		@Inject(forwardRef(() => ChatParticipantsService))
 		private participantService: ChatParticipantsService
 	) {}
+
+	private readonly logger: Logger = new Logger("Chat Service");
 
 	fetchChats(): Promise<ChatEntity[]> {
 		return this.chatRepository.find();
@@ -133,6 +135,7 @@ export class ChatsService {
 		const user2 = await this.getUserToCreateChatRoomOrFail(chatDetails.userID2);
 
 		const chatRoomName = this.generateDMName([user1.username, user2.username]);
+		this.logger.debug(`[Create DM] DM name: ${chatRoomName}`);
 		await this.checkChatRoomWithNameCanBeCreated(chatRoomName, true);
 
 		const newChat = this.chatRepository.create({
@@ -234,7 +237,9 @@ export class ChatsService {
 				`'${chatRoomName}': Chat name cannot start with "DM:"`
 			);
 		}
-		await this.validationService.validateChatRoomName(chatRoomName);
+		if (!isDM) {
+			await this.validationService.validateChatRoomName(chatRoomName);
+		}
 	}
 
 	private async getUserToCreateChatRoomOrFail(
