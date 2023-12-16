a277e71afefd2d5d792ed27a9253a9018ffa5622
mcombeau <mcombeau@student.42.fr>
2023-12-16 15:07:51 +0100

    change darkmode color for messages separator

--- frontend-app/src/components/styles/separator.tsx
+++ frontend-app/src/components/styles/separator.tsx
@@ -1,12 +1,16 @@
-// TODO: Enable darkmode on this???
 export function separatorLine(
 	title: string,
 	textColor: string = "darkblue",
 	backgroundColor: string = "lightblue"
 ) {
-	const bgLine: string = `bg-${textColor} dark:bg-dark${textColor}`;
+	let dmTextColor = "darkblue";
+	if (textColor == "sage") {
+		dmTextColor = "teal";
+	}
+
+	const bgLine: string = `bg-${textColor} dark:bg-dark${dmTextColor}`;
 	const bgText: string = `bg-${backgroundColor} dark:bg-dark${backgroundColor}`;
-	const textText: string = `text-${textColor} dark:text-dark${textColor}`;
+	const textText: string = `text-${textColor} dark:text-dark${dmTextColor}`;
 	return (
 		<div className={`w-full flex justify-center mb-6 relative mt-4`}>
 			<hr className={`${bgLine} border-0 h-0.5 w-3/4 mt-2.5`}></hr>
