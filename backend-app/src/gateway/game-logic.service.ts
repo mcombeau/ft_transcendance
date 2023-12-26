30f81b672b8c1fd371ca086a4e6e003a974d9ece
Mathilde KAPLOUN <mkaploun@made-f0Cr2s1.clusters.42paris.fr>
2023-12-26 13:52:53 +0100

    change speed game

--- backend-app/src/gateway/game-logic.service.ts
+++ backend-app/src/gateway/game-logic.service.ts
@@ -6,7 +6,7 @@ import {
 } from "./game.gateway.service";
 
 export const WINNING_SCORE = 10;
-export const GAME_SPEED = 3;
+export const GAME_SPEED = 4;
 
 const TERRAIN_HEIGHT = 400;
 const TERRAIN_WIDTH = 700;
